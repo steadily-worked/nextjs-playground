65ddca3014c5e5cadf1632219f5182bb479b6b21
박상민 <iamsangminpark@gmail.com>
2021-09-01 21:11:51 +0900

    apply GraphQL

---
+++ localapi/server/src/schema/user.js
@@ -0,0 +1,14 @@
+import { gql } from "apollo-server-express";
+
+const userSchema = gql`
+  type User {
+    id: ID!
+    nickname: String!
+  }
+  extend type Query {
+    users: [User!]!
+    user(id: ID!): User
+  }
+`;
+
+export default userSchema;
