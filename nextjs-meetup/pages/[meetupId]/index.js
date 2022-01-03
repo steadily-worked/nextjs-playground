8fc2f8d72aa73af3ee8fce7314d90bc2e1907d73
박상민 <iamsangminpark@gmail.com>
2022-01-03 21:09:50 +0900

    feat: MongoDB 액세스 및 데이터 불러오기

--- nextjs-meetup/pages/[meetupId]/index.js
+++ nextjs-meetup/pages/[meetupId]/index.js
@@ -1,34 +1,44 @@
-import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
-      title="First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
+      //   image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      //   title="First Meetup"
+      //   address="Some Street 5, Some City"
+      //   description="This is a first meetup"
     />
   );
 };
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:4321@cluster0.nzqrp.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  // 첫번째 {}: 모든객체를 가져옴
+  // 두번째 {}: 모든 문서가 추출되어야 하는 필드를 정의
+  // "_id: 1": id만 포함하고 다른 필드값은 포함하지 않는다.
+
+  client.close();
+
   return {
     fallback: false,
     // false: 지정되는 모든 paths를 정의함
     // true: 특정 path만 정의함 (pre-generate)
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+      // 이후에 들어가면
+    })),
   };
 }
 
@@ -36,19 +46,37 @@ export async function getStaticProps(context) {
   // fetch data for a single meetup
 
   const meetupId = context.params.meetupId;
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:4321@cluster0.nzqrp.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
 
   console.log(meetupId);
+
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-        id: meetupId,
-        title: "First Meetup",
-        address: "Some Street 5, Some City",
-        description: "This is a first meetup",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
+      //   {
+      //     image:
+      //       "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+      //     id: meetupId,
+      //     title: "First Meetup",
+      //     address: "Some Street 5, Some City",
+      //     description: "This is a first meetup",
+      //   },
     },
   };
 }
