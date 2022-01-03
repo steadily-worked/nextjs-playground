9da51d2d9cd10c9232e08429b79194251a17dbff
박상민 <iamsangminpark@gmail.com>
2022-01-03 20:28:02 +0900

    add: API response

--- nextjs-meetup/pages/new-meetup/index.js
+++ nextjs-meetup/pages/new-meetup/index.js
@@ -1,10 +1,24 @@
 // our-domain.com/new-meetup
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/dist/client/router";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
-  };
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
+  }
+  // 페이지 이동시 수집한 데이터 가져옴
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 
